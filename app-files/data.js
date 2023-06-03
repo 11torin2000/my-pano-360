1f9daaf8000f994bc2730bfaab107dc4f6ea8dd5
11torin2000 <yrazahid@gmail.com>
2023-06-03 22:56:41 +0200

    Pano360DR

---
+++ app-files/data.js
@@ -0,0 +1,103 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-frdtejnpan3d14",
+      "name": "FrýdštejnPan3D14",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1285,
+      "initialViewParameters": {
+        "yaw": 1.3378582252995077,
+        "pitch": 0,
+        "fov": 1.3843012993600772
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.40615237949801397,
+          "pitch": 0.23920363421403223,
+          "rotation": 0,
+          "target": "1-frdtejnpan3d"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-frdtejnpan3d",
+      "name": "FrýdštejnPan3D",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3927,
+      "initialViewParameters": {
+        "yaw": 0.0006164689999987871,
+        "pitch": 0,
+        "fov": 1.3843012993600772
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0587098522124272,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "0-frdtejnpan3d14"
+        },
+        {
+          "yaw": 3.0155801477851902,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "0-frdtejnpan3d14"
+        },
+        {
+          "yaw": 3.0161920138853606,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "0-frdtejnpan3d14"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
